e3cfd445e2306e9b92108d3fb88880c9c83c6708
ericgcc <serck@outlook.com>
2013-08-15 15:25:23 -0500

    Porgrama a resolver

--- MatFuncion.c
+++ MatFuncion.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#define R 6
+#define C 6
 
-void rellenar(int []);
-void imprimir(int []);
+void rellenar(int [][C]);
+void imprimir(int [][C]);
 
 void main(){
-	int a[6] = {0};
+	int a[R][C] = {0};
 	rellenar(a);
 	imprimir(a);	
 }
 
-void rellenar(int a [6]){
-	int i;
-	
-	for(i = 0; i <6 ; i++)
-		a[i] = rand() % 10;
+void rellenar(int a [R][C]){
+	int i, j;
+	srand(time(NULL));
+	for(i = 0; i <R ; i++){
+		for(j=0; j<C; j++){
+			a[i][j] = rand() % 10;
+		}
+	}
 }
 
-void imprimir(int a [6]){
-	int i;
-	
-	for(i = 0; i <6 ; i++)
-		printf("%d ", a[i]);
+void imprimir(int a [R][C]){
+	int i,j;	
+	for(i = 0; i <R ; i++){
+		for(j=0; j<C; j++){
+			printf("%d\t", a[i][j]);
+		}
+		puts("");
+	}
 }
 
 
